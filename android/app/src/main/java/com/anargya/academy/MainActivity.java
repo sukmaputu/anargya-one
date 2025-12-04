d1e19dda6f46fd5cefca4c58c31a12a5628b36b8
I Putu Sukma Widyantara <68043141+sukmaputu@users.noreply.github.com>
2025-12-04 05:22:39 -0800

    first commit

---
+++ android/app/src/main/java/com/anargya/academy/MainActivity.java
@@ -0,0 +1,5 @@
+package com.anargya.academy;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  base: "/anargya-one/", // <-- WAJIB untuk GitHub Pages
+})
